3dfeaed90abed42a1c9a5dfb9b21d15093c54148
Raz-Dahan <razdahan31@gmail.com>
2023-07-30 13:44:35 +0300

    Add outbound rule

--- Terraform/aws_ec2_with_ssh.tf
+++ Terraform/aws_ec2_with_ssh.tf
@@ -40,6 +40,13 @@ resource "aws_security_group" "security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "test" {
